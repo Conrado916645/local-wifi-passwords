0a1bd88eeaa9616bec434bb51d26b50c2665ccdd
Conrado916645 <36912212+Conrado916645@users.noreply.github.com>
2020-05-04 11:03:16 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,17 @@
+import subprocess
+
+import csv
+
+with open('wifi-passwords.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SSID","PASSWORD"])
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in data if "User Profile" in i]
+
+    for i in profiles:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+        results = [b.split(":")[1][1:-1] for b in results if "Key" in b]
+        try:
+            writer.writerow([i,results[0]])
+        except IndexError:
+            writer.writerow([i,""])
